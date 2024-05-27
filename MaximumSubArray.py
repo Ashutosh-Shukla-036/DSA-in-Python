00c0bd6c562e560b3f1152f92d358287ebfeb1c7
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-05-27 21:20:45 +0530

    modified

--- MaximumSubArray.py
+++ MaximumSubArray.py
@@ -18,3 +18,33 @@ def SubArray(arr):
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 print(SubArray(nums))
+
+import sys
+def MaxSubArray(arr):
+    maxi = -sys.maxsize - 1
+    sum = 0
+    start = 0
+    ansStart , ansEnd = 0,0
+
+    for i in range(len(arr)):
+            
+        sum += arr[i]
+
+        if sum > maxi:
+            maxi = sum
+
+            ansStart = start
+            ansEnd = i
+
+        if sum < 0:
+            sum = 0
+            start = i+1
+    print("maximum sum is:",maxi)
+    print("SubArray is [",end=" ")
+    for i in range(ansStart,ansEnd+1):
+        print(arr[i],end=" ")
+    print("]")
+
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+MaxSubArray(nums)     
